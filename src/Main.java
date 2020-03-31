a0ce69c8d7712191fb2db2937d344f8476632028
Jan Carlo De León <62573884+jancarlo-deleon@users.noreply.github.com>
2020-03-31 13:20:26 -0600

    Update Main.java
    Corrección de mal ingreso de datos.

--- src/Main.java
+++ src/Main.java
@@ -34,9 +34,9 @@ public class Main {
         System.out.println("Cambote, Zona 11, Huehuetenango");
         System.out.println("3130-8640");
         
-        Sistema . a cabo . println ( " Juan Amilcar Larias Méndez " );
-       Sistema . a cabo . println ( " Sector 3, cambote zona 11, Huehuetenango " );
-       Sistema . a cabo . println ( " 5737-5464 " );        
+        System.out.println ( " Juan Amilcar Larias Méndez " );
+        System.out.println ( " Sector 3, cambote zona 11, Huehuetenango " );
+        System.out.println ( " 5737-5464 " );        
     }
     
 }
